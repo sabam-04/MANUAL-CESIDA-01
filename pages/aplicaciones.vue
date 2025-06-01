32b928883e4577b341d62da5e85109f7f44c0870
sabam-04 <sandra.barrios3@esdmadrid.es>
2025-06-01 19:16:19 +0200

    color y tipo jason

--- pages/aplicaciones.vue
+++ pages/aplicaciones.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+import Card from '~/components/Card.vue'
+import cards from '~/data/aplicaciones.json'
 </script>
 
 <template>
- 
+ <div>
+    <Card v-for="(card, i) in cards" :key="i" v-bind="card" />
+  </div>
 </template>
 
 <style scoped lang="scss">
--- pages/color.vue
+++ pages/color.vue
@@ -1,12 +1,14 @@
 <script setup>
-
+import Card from '~/components/Card.vue'
+import cards from '~/data/color.json'
 </script>
 
 <template>
- 
+ <div>
+    <Card v-for="(card, i) in cards" :key="i" v-bind="card" />
+  </div>
 </template>
 
 <style scoped lang="scss">
 
 </style>
-
